335c3656d1bb3685030a54460f5aa88cfedad878
Pramod Subramanyan <pramod.subramanyan@gmail.com>
2019-07-01 15:19:34 +0530

    Deleting autogenerated files.

--- src/obj_dir/Voc8051_tb__ALLcls.d
+++
@@ -1,7 +0,0 @@
-Voc8051_tb__ALLcls.o: Voc8051_tb__ALLcls.cpp Voc8051_tb.cpp Voc8051_tb.h \
- /usr/local/share/verilator/include/verilated_heavy.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilatedos.h Voc8051_tb__Inlines.h \
- /usr/local/share/verilator/include/verilated.h Voc8051_tb__Syms.h \
- Voc8051_tb_S.h Voc8051_tb_S.cpp
--- src/obj_dir/Voc8051_tb__ALLsup.d
+++
@@ -1,7 +0,0 @@
-Voc8051_tb__ALLsup.o: Voc8051_tb__ALLsup.cpp Voc8051_tb__Syms.cpp \
- Voc8051_tb__Syms.h /usr/local/share/verilator/include/verilated_heavy.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilatedos.h Voc8051_tb.h \
- Voc8051_tb__Inlines.h /usr/local/share/verilator/include/verilated.h \
- Voc8051_tb_S.h
